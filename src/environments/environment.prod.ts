b84ff4e85ae4f7ddf7965eed424bfa0b7a881a3d
Ermindo Treviso Neto <126285198+protileon-ermes@users.noreply.github.com>
2023-11-09 09:02:34 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyARzxnsAuip6HuCztq54S5eTi6QfZNWKZc",
+    authDomain: "contatos2024-9a933.firebaseapp.com",
+    projectId: "contatos2024-9a933",
+    storageBucket: "contatos2024-9a933.appspot.com",
+    messagingSenderId: "508824172876",
+    appId: "1:508824172876:web:cc48a66e3fe0072f7f77e9"
+  }
+};
